9e33cd6c4982f9585fef740a7285b81897ce36d6
Adam Laki <info@adamlaki.com>
2024-03-02 16:03:47 +0100

    Fix TOC

--- .eleventy.js
+++ .eleventy.js
@@ -185,7 +185,7 @@ module.exports = config => {
     elements.forEach(element => {
       const heading = element.querySelector('h2, h3');
 
-      if (heading && heading.textContent !== 'Example(s)' && heading.textContent !== 'Argument(s)') {
+      if (heading && heading.textContent !== 'Example(s)' && heading.textContent !== 'Argument(s)' && heading.textContent !== 'References') {
         toc += `<li class="toc-level-${heading.tagName.toLowerCase()}"><a href="#${heading.id}">${heading.textContent}</a></li>`;
       }
     });
