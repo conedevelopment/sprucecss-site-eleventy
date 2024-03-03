ca73fde668d04c92dc43c5356a1155043413e002
Adam Laki <info@adamlaki.com>
2024-03-03 15:53:32 +0100

    Add API response

--- src/_data/ui.js
+++ src/_data/ui.js
@@ -1,7 +1,28 @@
 const EleventyFetch = require('@11ty/eleventy-fetch');
 
-module.exports = async (config) => {
+module.exports = async () => {
   const url = process.env.UI_API_URL;
+
+  if (!url) {
+    const data = [
+      {
+        title: 'Introduction',
+        folder: 'getting-started',
+        slug: 'introduction',
+        order: 1,
+        content: 'Etiam semper luctus nisi sodales commodo. Proin a diam ex. Quisque id arcu turpis. Proin a libero vel quam hendrerit tristique ut et justo.',
+      },
+      {
+        title: 'Config',
+        folder: 'getting-started',
+        slug: 'config',
+        order: 2,
+        content: 'Donec commodo nibh at suscipit gravida. Suspendisse accumsan blandit velit id finibus. Morbi hendrerit mi id quam egestas, non pulvinar massa sollicitudin.',
+      },
+    ];
+
+    return data;
+  }
 
   const response = await EleventyFetch(url, {
     duration: '0s',
