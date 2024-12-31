601ba4ac4c850e4e5551074c2350a606c3c31d01
Adam Laki <info@adamlaki.com>
2024-12-31 22:18:49 +0100

    Cleanup

--- src/js/sidebar-navigation.js
+++ src/js/sidebar-navigation.js
@@ -1,12 +1,11 @@
 (() => {
   const sidebar = document.querySelector('.l-documentation__sidebar');
-  const activeItem = sidebar.querySelector('[aria-current="page"]');
 
   if (!sidebar) {
     return;
   }
 
-  console.log(sidebar.scrollTop, activeItem.offsetTop);
+  const activeItem = sidebar.querySelector('[aria-current="page"]');
 
   if (activeItem && activeItem.offsetTop > 200) {
     sidebar.scrollTop = activeItem.offsetTop;
